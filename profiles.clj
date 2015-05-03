6f07749374a232e6cb660444ea01df116e2e11a1
Christopher M. Hobbs <cmhobbs@member.fsf.org>
2015-05-03 17:27:59 -0500

    Update usernames in project.clj and profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -2,7 +2,7 @@
  :dev {
        :env {
              :dev true
-             :database-url "jdbc:postgresql://localhost/madetomeasure_development?user=matthewkirk"
+             :database-url "jdbc:postgresql://dev:madetomeasure@localhost/madetomeasure_development""
              }
        :dependencies [[ring-mock "0.1.5"]
                       [ring/ring-devel "1.3.2"]
@@ -18,7 +18,7 @@
        }
  :production 
  {
-  :env {:database-url "jdbc:postgresql://localhost/madetomeasure?user=matthewkirk"}
+  :env {:database-url "jdbc:postgresql://localhost/madetomeasure?user=deploy"}
   }
  }
 
